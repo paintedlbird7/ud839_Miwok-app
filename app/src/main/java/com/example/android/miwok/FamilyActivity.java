c4f88ac948335ca974f0adfce91524906a4c2ca1
paintedlbird7 <rosaperezlarios@gmail.com>
2018-07-19 13:40:46 -0700

    Miwok App (fragments working)

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -31,11 +31,11 @@ public class FamilyActivity extends AppCompatActivity {
         // Create an array of words
         // words.add("one");
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("father", "әpә", R.drawable.family_mother, R.raw.family_mother));
-        words.add(new Word("mother", "әṭa", R.drawable.family_father, R.raw.family_father));
-        words.add(new Word("son", "angsi", R.drawable.family_daughter, R.raw.family_daughter));
-        words.add(new Word("daughter", "tune", R.drawable.family_son, R.raw.family_son));
-        words.add(new Word("grandmother", "ama", R.drawable.family_older_sister, R.raw.family_older_sister));
+        words.add(new Word("father", "әpә", R.drawable.family_father, R.raw.family_father));
+        words.add(new Word("mother", "әṭa", R.drawable.family_mother, R.raw.family_mother));
+        words.add(new Word("son", "angsi", R.drawable.family_son, R.raw.family_son));
+        words.add(new Word("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new Word("grandmother", "ama", R.drawable.family_grandmother, R.raw.family_grandmother));
 
 
         // Create a new {@link TextView}
--- app/src/main/java/com/example/android/miwok/FamilyFragment.java
+++ app/src/main/java/com/example/android/miwok/FamilyFragment.java
@@ -88,11 +88,11 @@ public class FamilyFragment extends Fragment {
 
         // Create a list of words
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("father", "әpә", R.drawable.family_mother, R.raw.family_mother));
-        words.add(new Word("mother", "әṭa", R.drawable.family_father, R.raw.family_father));
-        words.add(new Word("son", "angsi", R.drawable.family_daughter, R.raw.family_daughter));
-        words.add(new Word("daughter", "tune", R.drawable.family_son, R.raw.family_son));
-        words.add(new Word("grandmother", "ama", R.drawable.family_older_sister, R.raw.family_older_sister));
+        words.add(new Word("father", "әpә", R.drawable.family_father, R.raw.family_father));
+        words.add(new Word("mother", "әṭa", R.drawable.family_mother, R.raw.family_mother));
+        words.add(new Word("son", "angsi", R.drawable.family_son, R.raw.family_son));
+        words.add(new Word("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
+        words.add(new Word("grandmother", "ama", R.drawable.family_grandmother, R.raw.family_grandmother));
 
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
